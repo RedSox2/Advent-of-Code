fc67a1fc80322bf0a4028d0ca3dcdcc5e61992c4
RedSox2 <samir.z.warner@gmail.com>
2022-12-28 15:24:48 -0800

    solved 2020/day3

--- 2020/day3/main.py
+++ 2020/day3/main.py
@@ -13,15 +13,16 @@ print(p1)
 
 # part 2
 p2 = 1
-slopes = [(1, 1), (3, 1), (5, 1), (7, 1)]
-for d, _ in slopes: 
-	c = d 
+slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+for d, x in slopes: 
+	c = 0 
 	COLS = len(m[0])
 	a = 0
-	for i in m[1:]: 
+	for i in m[::x]: 
 		if i[c%COLS] == '#': 
 			a += 1
-		c += d	
+		c += d
+		if x == 2: print(c, m.index(i))
 	p2 *= a
-for i in range(1, len(i), 2): 
-	
+	print(a)
+print(p2)
