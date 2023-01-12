120019d8e93b2a83341c1b95d00b0c31725cc64c
Replit user <>
2023-01-12 17:59:12 +0000

    solve 2022 day13

--- .vimrc
+++ .vimrc
@@ -1,6 +1,9 @@
-colo koehler
 syntax on
 inoremap <C-O> <Esc>o
+inoremap <C-J> <Down>
+inoremap <C-H> <Left>
+inoremap <C-K> <Up>
+inoremap <C-L> <Right>
 set mouse=a
 set number
 set autoindent
--- 2022/day13/main.py
+++ 2022/day13/main.py
@@ -1,27 +1,55 @@
 with open('input.txt', 'r') as file: 
-	pairs = file.read().strip().split('\n\n')
+	f = file.read().strip()
+	pairs = [i.strip() for i in f.split('\n\n')]
+	lines = [i.strip() for i in f.split()]
 
 pairs = [i.split('\n') for i in pairs]
 
-def flatten(S):
-	if S == []:
-		return S
-	if isinstance(S[0], list):
-		return flatten(S[0]) + flatten(S[1:])
-	return S[:1] + flatten(S[1:])
+def compare(left, right): 
+	while len(left) > 0 and len(right) > 0:
+		l, r = left.pop(0), right.pop(0)
+
+		if type(l) == int and type(r) == int: 
+			if l < r: 
+				return 1
+			elif l > r: 
+				return 0
+
+		elif type(l) == list and type(r) == int: 
+			if (a := compare(l, [r])) != -1: 
+				return a
+		elif type(l) == int and type(r) == list: 
+			if (a := compare([l], r)) != -1: 
+				return a
+		elif type(l) == list and type(r) == list: 
+			if (a := compare(l, r)) != -1: 
+				return a
+	if len(left) < len(right): 
+		return 1
+	elif len(left) > len(right): 
+		return 0
+	else: 
+		return -1
+
 
 total = 0
-print(len(pairs))
-for n, parts in enumerate(pairs): 
-	left, right = parts
-	left, right = flatten(eval(left)), flatten(eval(right))
-
-	for i in range(len(right)): 
-		try: 
-			if left[i] < right[i]: 
-				total += n+1
-				break
-		except IndexError: 
-			total += n+1
-			break
+for n, (a, b) in enumerate(pairs): 
+	if compare(eval(a), eval(b)) == 1: 
+		total += n + 1
 print(total)
+
+decoder1 = [[2]]
+decoder2 = [[6]]
+
+ind1, ind2 = 1, 2
+
+while len(lines) > 0: 
+	l = lines.pop(0)
+
+	if len(l) == 0: 
+		continue
+	if compare(eval(l), [[2]]) == 1: 
+		ind1 += 1
+	if compare(eval(l), [[6]]) == 1: 
+		ind2 += 1
+print(ind1, ind2, ind1*ind2)
